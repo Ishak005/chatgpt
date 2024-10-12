efcc822f4145bc4015010b1edc205551c992b1b4
Ishak005 <ishaktwahir@gmail.com>
2024-10-12 22:19:27 +0300

    add files

---
+++ chatgpt-clone/src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="sidebar">
+        <div className="upperSide">
+        <div className="upperSideTop"><img src="" alt="logo" /><span className="brand">ChatGPT</span></div>
+          <button className="midBtn"><img src="" alt="addImg" className="addBtn"/>New Chat</button>
+        </div>
+        <div className="lowerSide"></div>
+      </div>
+      <div className="main">
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
